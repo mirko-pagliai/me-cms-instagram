6ca0e8e39fef11a1cf07ec8f3c040a3a22de534a
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-04-02 18:02:12 +0200

    deleted API

--- API/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","MeInstagram\\Controller\\InstagramController"],["c","MeInstagram\\Shell\\InstallShell"],["c","MeInstagram\\Utility\\Instagram"],["c","MeInstagram\\View\\Cell\\PhotosCell"]];
